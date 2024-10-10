c259bb9f58bdd27794fddd04dea8641d8ec7a759
KeshavKumarCG <keshav.kumar@cginfinity.com>
2024-10-10 14:10:28 +0530

    Initial commit on login-functionality branch

--- obj/Release/net8.0/Backend.AssemblyInfo.cs
+++ obj/Release/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30380ca8ac30a17f783b8a5ce14de3204d13c574")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
