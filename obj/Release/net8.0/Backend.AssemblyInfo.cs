29dc01845a364504eac69ba610c7573139f0f262
GursimranKaurCG <Gursimran.Kaur@cginfinity.com>
2024-10-17 16:13:47 +0530

    Update cache files

--- obj/Release/net8.0/Backend.AssemblyInfo.cs
+++ obj/Release/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a5cc65e8b3dfd43a13b2a39d4274a9894a5ecb9")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20f21406da2e7df99f9a3a4e3129f628cc1a91ba")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
