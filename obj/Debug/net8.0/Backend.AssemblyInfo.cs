33b6e01d8c13cb39e08c9bf4c9dc8abc73b80979
KeshavKumarCG <keshav.kumar@cginfinity.com>
2024-10-17 15:21:22 +0530

    stagged Authentication

--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ac9f05d9909736bafa39b64743bb4639c24f0a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c98bf42c25d7af6028fb89763565bbc81e98279")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
