64d05a2858050c88f0edbc7469b030882ce006d8
KeshavKumarCG <keshav.kumar@cginfinity.com>
2024-10-21 11:17:51 +0530

    obj/Debug updates

--- obj/Release/net8.0/Backend.AssemblyInfo.cs
+++ obj/Release/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20f21406da2e7df99f9a3a4e3129f628cc1a91ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3a0b3b3543d51a39fe77a630e4451ad41712558")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
