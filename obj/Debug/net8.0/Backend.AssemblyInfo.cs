03804fec46771097e24254be69feb67efc28582c
KeshavKumarCG <keshav.kumar@cginfinity.com>
2024-10-21 11:47:21 +0530

    automatic-updates resolved

--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3a0b3b3543d51a39fe77a630e4451ad41712558")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64d05a2858050c88f0edbc7469b030882ce006d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
