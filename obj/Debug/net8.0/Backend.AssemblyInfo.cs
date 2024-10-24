ae83e2f5a0188cba50b0ff280a4b6a7c2256584f
Cgshantanu <shantanu.sahoo@cginfinity.com>
2024-10-24 12:05:35 +0530

    SMS-API-and-Twilio-Added-successfully

--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03c6bae7c2d225207ed923d9417541eb05b5b94f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+136e430c1a5d2a182f35e4e52c4591f7cb191449")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
