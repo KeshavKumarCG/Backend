bcb7b35ca4f425b00375c4d7b5a5bcee09c0247c
Harshdeep Singh <harshdeep.singh@cginfinity.com>
2024-11-05 10:11:35 +0530

    Add Twilio configuration and update project dependencies

--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3be4b90d65cb774157e5ed264e38f6e7cfb5dc2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8448f60ca6527b7fd0e325596a4907852620bab6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
