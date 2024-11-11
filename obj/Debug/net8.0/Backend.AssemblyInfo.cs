cf9643bb49902b525ae7d439703baf4202488480
KeshavKumarCG <keshav.kumar@cginfinity.com>
2024-11-11 11:46:10 +0530

    Update .gitignore to include FirebaseAdmin.json and update assembly informational version

--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26fdf2c3338474ce96f413163a533e0c6f4fed96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b19b414c5c24a48d58be396c653ac573443294c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
