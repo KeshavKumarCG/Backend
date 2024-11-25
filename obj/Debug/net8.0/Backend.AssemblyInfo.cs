b8fd0f1626d8bb25e7054f60259e029dc6329ce8
KeshavKumarCG <keshav.kumar@cginfinity.com>
2024-11-25 20:18:19 +0530

    changes

--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b565e5076306f61fd1f54385f3d24462cb927249")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40f0481c353a763150882315e35dd68ffb16cec1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
