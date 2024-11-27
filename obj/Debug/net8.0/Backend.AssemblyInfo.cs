8292785a5d01f247f050b3ffdf565f2c23215f7e
KeshavKumarCG <keshav.kumar@cginfinity.com>
2024-11-27 14:43:31 +0530

    update assembly informational version and cache inputs

--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8fd0f1626d8bb25e7054f60259e029dc6329ce8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d524881421da7edbf3b1f5d872bdfaf1dcf8344")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
